c222637911284e3d86c9eb2aaccfb1bb8c558d36
Irina M. S <irinamardhatillah@gmail.com>
2024-02-11 19:58:14 +0700

    Implement the backward function

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ class Value:
     def __init__(self, data, _children=(), _operation="", label=""):
         self.data = data
         self.grad = 0.0
+        self._backward = lambda: None
         self._prev = _children
         self._op = _operation
         self.label = label
@@ -13,17 +14,34 @@ class Value:
         return f"Value(data={self.data})"
 
     def __add__(self, other):
+        def _backward():
+            self.grad = 1.0 * output.grad
+            other.grad = 1.0 * output.grad
+
         output = Value(self.data + other.data, (self, other), "+")
+        output._backward = _backward
+
         return output
 
     def __mul__(self, other):
+        def _backward():
+            self.grad = other.data * output.grad
+            other.grad = self.data * output.grad
+
         output = Value(self.data * other.data, (self, other), "*")
+        output._backward = _backward
+
         return output
 
     def tanh(self):
+        def _backward():
+            self.grad = (1 - tanh**2) * output.grad
+
         x = self.data
         tanh = (math.exp(2 * x) - 1) / (math.exp(2 * x) + 1)
         output = Value(tanh, (self,), "tanh")
+        output._backward = _backward
+
         return output
 
 
@@ -39,10 +57,28 @@ if __name__ == "__main__":
     # The neuron's bias
     b = Value(6.7)
 
+    # Forward pass
     x1w1 = x1 * w1
     x2w2 = x2 * w2
     xw = x1w1 + x2w2
     n = xw + b
     o = n.tanh()
 
-    print(o)
+    # Backward pass
+    o.grad = 1
+    o._backward()
+    n._backward()
+    xw._backward()
+    x2w2._backward()
+    x1w1._backward()
+
+    print(f"Gradient of o: {o.grad}")
+    print(f"Gradient of n: {n.grad}")
+    print(f"Gradient of xw: {xw.grad}")
+    print(f"Gradient of b: {b.grad}")
+    print(f"Gradient of x1w1: {x1w1.grad}")
+    print(f"Gradient of x2w2: {x2w2.grad}")
+    print(f"Gradient of x1: {x1.grad}")
+    print(f"Gradient of w1: {w1.grad}")
+    print(f"Gradient of x2: {x2.grad}")
+    print(f"Gradient of w2: {w2.grad}")
