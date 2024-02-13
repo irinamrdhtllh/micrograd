11ee24755e93093b215f86e20e9c27d5a318c250
Irina M. S <irinamardhatillah@gmail.com>
2024-02-13 23:10:10 +0700

    Implement gradient descent to update the parameters

--- main.py
+++ main.py
@@ -6,13 +6,21 @@ inputs = [[2.0, 3.0, -1.0], [3.0, -1.0, 0.5], [0.5, 1.0, 1.0], [1.0, 1.0, -1.0]]
 targets = [1.0, -1.0, -1.0, 1.0]
 nn = MLP(3, [4, 4, 1])
 
-pred_targets = [nn(x) for x in inputs]
+for iter in range(1000):
+    # Forward pass
+    pred_targets = [nn(x) for x in inputs]
+    loss = sum(
+        ((y_pred - y_true) ** 2 for y_true, y_pred in zip(targets, pred_targets)),
+        start=Value(0),
+    )
 
-loss = sum(
-    ((y_pred - y_true) ** 2 for y_true, y_pred in zip(targets, pred_targets)),
-    start=Value(0),
-)
+    # Backward pass
+    for param in nn.parameters():
+        param.grad = 0.0
+    loss.backward()
 
-loss.backward()
+    # Update parameters
+    for param in nn.parameters():
+        param.data += -0.05 * param.grad
 
-print(nn.parameters())
+    print(f"iter: {iter + 1}, loss: {loss.data}")
