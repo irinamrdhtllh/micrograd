d5f6e5589408f57faf7f09a4c5e73c6e3acf7c2c
Irina M. S <irinamardhatillah@gmail.com>
2024-02-13 22:36:50 +0700

    MLP neural network example

--- main.py
+++ main.py
@@ -1,35 +1,18 @@
 from micrograd.engine import Value
+from micrograd.layers import MLP
 
 
-# The neuron's inputs
-x1 = Value(2.0)
-x2 = Value(0.0)
+inputs = [[2.0, 3.0, -1.0], [3.0, -1.0, 0.5], [0.5, 1.0, 1.0], [1.0, 1.0, -1.0]]
+targets = [1.0, -1.0, -1.0, 1.0]
+nn = MLP(3, [4, 4, 1])
 
-# The neuron's weights
-w1 = Value(-3.0)
-w2 = Value(1.0)
+pred_targets = [nn(x) for x in inputs]
 
-# The neuron's bias
-b = Value(6.7)
+loss = sum(
+    ((y_pred - y_true) ** 2 for y_true, y_pred in zip(targets, pred_targets)),
+    start=Value(0),
+)
 
-# Forward pass
-x1w1 = x1 * w1
-x2w2 = x2 * w2
-xw = x1w1 + x2w2
-n = xw + b
-exp = (2 * n).exp()
-o = (exp - 1) / (exp + 1)
+loss.backward()
 
-# Backward pass
-o.backward()
-
-print(f"Gradient of o: {o.grad}")
-print(f"Gradient of n: {n.grad}")
-print(f"Gradient of xw: {xw.grad}")
-print(f"Gradient of b: {b.grad}")
-print(f"Gradient of x1w1: {x1w1.grad}")
-print(f"Gradient of x2w2: {x2w2.grad}")
-print(f"Gradient of x1: {x1.grad}")
-print(f"Gradient of w1: {w1.grad}")
-print(f"Gradient of x2: {x2.grad}")
-print(f"Gradient of w2: {w2.grad}")
+print(nn.layers[0].neurons[0].weights[0].grad)
